fbfbf6c6089c53ceb46379f9dc6a2e109e98ef90
sin-bit0731 <wissenschaft.kreis0731@gmail.com>
2023-09-23 17:04:33 +0900

    Itemsテーブルのカラムの追加とバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :ship_area
+  belongs_to :ship_day
+  belongs_to :ship_method
+  belongs_to :user
+
+  validates :item_name, :description, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_method_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
