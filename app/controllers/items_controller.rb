b8a4b1354479c0081460df1c9aa34eb4056e265b
sin-bit0731 <wissenschaft.kreis0731@gmail.com>
2023-10-04 16:29:57 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
-    redirect_to root_path
+    render :edit, status: :unprocessable_entity
   end  
 
   private
